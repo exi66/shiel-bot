37b8b1a134e25fc1c7c05ad2dc6d72b961f9955b
exi <eternityexi@gmail.com>
2022-10-20 12:33:44 +0400

    mov setActivity to try-catch

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const where = __filename.slice(__dirname.length + 1);
 const error_here = where + "/error";
 const log_here = where + "/log";
 
-async() => {
+(async() => {
   const config = require("./config.json");
   const client = await require("./bot.js")(config);
   const scraper = require("./scraper.js")(client);
-}
+})();
