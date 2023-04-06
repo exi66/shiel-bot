f80ff751615f588bba96a14e612bcaa047f007b9
exi66 <eternityexi@gmail.com>
2023-04-06 22:43:37 +0400

    add module, that remove all slash commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -7,3 +7,4 @@ const rest = new REST({ version: '10' }).setToken(token);
 rest.put(Routes.applicationCommands(clientId), { body: [] })
 	.then(() => console.log('Successfully deleted all application commands.'))
 	.catch(console.error);
+  
